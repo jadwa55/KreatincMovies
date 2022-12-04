7c98f6550f0f314365044b6057496d0c961dcc7f
jadwa55 <75550231+jadwa55@users.noreply.github.com>
2022-12-04 01:20:59 +0100

    Cards

--- src/components/Cardtem.vue
+++ src/components/Cardtem.vue
@@ -2,15 +2,31 @@
   <div class="card">
     <div class="card-image">
       <!-- need to bind alt -->
-      <img src="../assets/imgs/hpotter.jpg" alt="movie name" />
+      <img :src="image" :alt="movieName" />
     </div>
     <div class="card-body">
-      <p class="movie-title">{{ movieName }}</p>
-      <span class="movie-rating">{{ movieRating }}</span>
+      <p class="movie-title">Name: {{ movieName }}</p>
+      <div class="movie-details">
+        <p class="movie-details-rating">Year: {{ year }}</p>
+        <p class="movie-details-rating">Type: {{ type }}</p>
+      </div>
     </div>
+    <router-link
+      class="movie-show"
+      :to="{ name: 'movieShow', params: { id: imdbId } }"
+    >
+      <button class="movie-show-btn">vew</button>
+    </router-link>
   </div>
 </template>
 
 <script setup>
-const props = defineProps(["movieName", "movieRating"]);
+const props = defineProps([
+  "movieName",
+  "movieRating",
+  "image",
+  "year",
+  "type",
+  "imdbId",
+]);
 </script>
