ad5899fab947058500bcb371836284e46aa68dae
jadwa55 <75550231+jadwa55@users.noreply.github.com>
2022-12-04 01:24:27 +0100

    import Router in app file

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,30 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/Cardtem.vue'
+import { ref, provide, readonly } from "vue";
+import { RouterLink, RouterView, useRoute } from "vue-router";
+const route = useRoute();
+const search = ref("best");
+
+provide("search", readonly(search));
 </script>
 
 <template>
   <header>
     <div class="wrapper">
       <nav class="navbar">
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
+        <div class="navbar-link">
+          <RouterLink to="/">IMDB</RouterLink>
+        </div>
+        <div class="navbar-search" v-if="route.name === 'home'">
+          <input
+            type="text"
+            v-model="search"
+            placeholder="Search for a movie"
+          />
+        </div>
       </nav>
     </div>
   </header>
-<div>
-  <RouterView />
-</div>
+  <div>
+    <RouterView />
+  </div>
 </template>
