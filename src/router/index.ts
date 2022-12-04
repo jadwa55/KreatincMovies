ef8564be467327c3e3b37aa976c3c500ea61c794
jadwa55 <75550231+jadwa55@users.noreply.github.com>
2022-12-04 01:27:27 +0100

    Router fixed

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,17 +1,22 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory } from "vue-router";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'test',
+      path: "/",
+      name: "home",
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import('../views/HomeView.vue')
-    }
-  ]
-})
+      component: () => import("../views/HomeView.vue"),
+    },
+    {
+      path: "/:id",
+      name: "movieShow",
+      component: () => import("../views/MovieInfo.vue"),
+    },
+  ],
+});
 
-export default router
+export default router;
