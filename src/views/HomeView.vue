88e3cb94cab08b0d7be50de28d6732369db31e5a
jadwa55 <75550231+jadwa55@users.noreply.github.com>
2022-12-04 01:25:49 +0100

    Home Page

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,17 +1,77 @@
-<script setup lang="ts">
-import { ref } from '@vue/reactivity'
-import Cardtem from '../components/Cardtem.vue'
-const movies = ref("")
-fetch('http://www.omdbapi.com/?s=batt&apikey=77323777')
-  .then((res) => res.json())
-  .then((json) => (movies.value = json))
+<script setup>
+import { ref, watch, inject } from "vue";
+import axios from "axios";
+import Cardtem from "../components/Cardtem.vue";
+import Pagination from "../components/Pagination.vue";
+import Loader from "../components/LoaderItem.vue";
+
+const API_KEY = import.meta.env.VITE_API_KEY;
+const search = inject("search");
+const movies = ref("");
+const page = ref(1);
+const loading = ref(false);
+
+const fetchMovies = async () => {
+  loading.value = true;
+  await axios
+    .get(`http://www.omdbapi.com`, {
+      params: { s: search.value, apikey: API_KEY, page: page.value },
+    })
+    .then((data) => {
+      movies.value = data.data;
+      loading.value = false;
+    });
+};
+const pageChanged = (p) => {
+  page.value = p;
+};
+watch(
+  search,
+  () => {
+    if (search.value.length > 2) {
+      page.value = 1;
+      fetchMovies();
+    }
+  },
+  { immediate: true }
+);
+watch(page, () => {
+  fetchMovies();
+});
 </script>
 
 <template>
   <main>
-    {{ movies }}
-    <div v-for="movie in movies" :key="movie.id">
-    <Cardtem :movieName="movie.Title" :movieRating="movie.imdbRating"/>
+    <div v-if="loading" class="loadar">
+      <Loader />
+    </div>
+    <div v-else>
+      <div class="search-section-title" v-if="!search">
+        <p>Search for a movie</p>
+      </div>
+      <div v-if="search && !movies.Search" class="noResults">
+        <img src="../assets/imgs/noResultsImage.png" alt="noResultsImage" />
+        <h2>no results found for {{ search }} !</h2>
+      </div>
+      <div v-else-if="search && movies.Search" class="cards-data">
+        <div v-for="movie in movies.Search" :key="movie.id">
+          <Cardtem
+            :movieName="movie.Title"
+            :movieRating="movie.imdbRating"
+            :image="movie.Poster"
+            :year="movie.Year"
+            :type="movie.Type"
+            :imdbId="movie.imdbID"
+          />
+        </div>
+      <div class="pagination" v-if="movies.Search">
+        <Pagination
+          :count="movies.totalResults"
+          :page="page"
+          @update:modelValue="pageChanged"
+        />
+      </div>
+      </div>
     </div>
   </main>
 </template>
